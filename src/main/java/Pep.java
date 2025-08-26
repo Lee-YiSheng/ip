d063b1318c644fc122d75bf1dbe8e779c292dabd
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-08-26 12:36:04 +0800

    Added Echo functionality
    Program now waits for user input, will echo the input unless receives "bye", where it then exists.

--- src/main/java/Pep.java
+++ src/main/java/Pep.java
@@ -1,11 +1,23 @@
+import javax.swing.*;
+import java.util.Scanner;
 public class Pep {
     public static void main(String[] args) {
         String chatbotName = "Pep";
-        System.out.println("____________________________________________________________");
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println(" Hello! I'm " + chatbotName);
         System.out.println(" I love gyming");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        String userInput =  scanner.nextLine();
+        while (!(userInput.equals("bye"))) {
+            System.out.println(line);
+            System.out.println(" " + userInput);
+            System.out.println(line);
+            userInput =  scanner.nextLine();
+        }
+        scanner.close();
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
     }
 }
