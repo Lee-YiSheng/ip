3e1f5f625b40234e1d79092db092d28102e39147
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-08-27 17:04:26 +0800

    Mark as Done
    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Pep.java
+++ src/main/java/Pep.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Pep {
@@ -14,15 +13,24 @@ public class Pep {
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
-
         ui.showWelcome(chatbotName);
 
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 ui.showTaskList(taskList);
+            } else if (userInput.startsWith("mark ")) {
+                int index = parseIndex(userInput);
+                if (index != -1 && taskList.markTask(index)) {
+                    ui.showMarked(taskList.getTask(index));
+                }
+            } else if (userInput.startsWith("unmark ")) {
+                int index = parseIndex(userInput);
+                if (index != -1 && taskList.unmarkTask(index)) {
+                    ui.showUnmarked(taskList.getTask(index));
+                }
             } else {
-                taskList.addTask(userInput);
+                taskList.addTask(new Task(userInput));
                 ui.showAdded(userInput);
             }
             userInput = scanner.nextLine();
@@ -32,11 +40,20 @@ public class Pep {
         scanner.close();
     }
 
+    private int parseIndex(String input) {
+        try {
+            return Integer.parseInt(input.split(" ")[1]) - 1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
         Pep pepBot = new Pep("Pep");
         pepBot.run();
     }
 
+    // ==================== Ui Class ====================
     public static class Ui {
         private final String line;
 
@@ -46,8 +63,8 @@ public class Pep {
 
         public void showWelcome(String name) {
             printLine();
-            System.out.println(" Hello! I'm " + name);
-            System.out.println(" What can I do for you?");
+            System.out.println(" Hi im" + name);
+            System.out.println(" What you want");
             printLine();
         }
 
@@ -59,15 +76,31 @@ public class Pep {
 
         public void showTaskList(TaskList taskList) {
             printLine();
+            System.out.println(" Here are the tasks in your list:");
             for (int i = 0; i < taskList.getCount(); i++) {
-                System.out.println(" " + (i + 1) + ". " + taskList.getTask(i));
+                Task task = taskList.getTask(i);
+                System.out.println(" " + (i + 1) + ".[" + task.getStatusIcon() + "] " + task.getDescription());
             }
+            printLine();
+        }
+
+        public void showMarked(Task task) {
+            printLine();
+            System.out.println(" task marked as done:");
+            System.out.println("   [" + task.getStatusIcon() + "] " + task.getDescription());
+            printLine();
+        }
+
+        public void showUnmarked(Task task) {
+            printLine();
+            System.out.println(" this task marked as not done:");
+            System.out.println("   [" + task.getStatusIcon() + "] " + task.getDescription());
             printLine();
         }
 
         public void showGoodbye() {
             printLine();
-            System.out.println(" Bye. Hope to see you again soon!");
+            System.out.println(" Bye. please dont come back");
             printLine();
         }
 
@@ -76,28 +109,72 @@ public class Pep {
         }
     }
 
+    // ==================== TaskList Class ====================
     public static class TaskList {
-        private final String[] tasks;
+        private final Task[] tasks;
         private int count;
 
         public TaskList() {
-            this.tasks = new String[100];
+            this.tasks = new Task[100];
             this.count = 0;
         }
 
-        public void addTask(String task) {
+        public void addTask(Task task) {
             if (count < tasks.length) {
                 tasks[count] = task;
                 count++;
             }
         }
 
-        public String getTask(int index) {
+        public boolean markTask(int index) {
+            if (index >= 0 && index < count) {
+                tasks[index].markAsDone();
+                return true;
+            }
+            return false;
+        }
+
+        public boolean unmarkTask(int index) {
+            if (index >= 0 && index < count) {
+                tasks[index].markAsNotDone();
+                return true;
+            }
+            return false;
+        }
+
+        public Task getTask(int index) {
             return tasks[index];
         }
 
         public int getCount() {
             return count;
+        }
+    }
+
+    // ==================== Task Class ====================
+    public static class Task {
+        private final String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsNotDone() {
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        public String getDescription() {
+            return description;
         }
     }
 }
