d5f09f1c5a28256bad6dc7add0fcfac98063bbc0
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-09-06 18:12:50 +0800

    add AddDeadlineCommand class

---
+++ src/main/java/AddDeadlineCommand.java
@@ -0,0 +1,16 @@
+public class AddDeadlineCommand extends Command {
+    private final String description;
+    private final String by;
+
+    public AddDeadlineCommand(String description, String by) {
+        this.description = description;
+        this.by = by;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) {
+        Deadline task = new Deadline(description, by);
+        tasks.addTask(task);
+        ui.showAdded(task.toString(), tasks.getCount());
+    }
+}
