ab762b75d6c65f0f5b2899d19a4533046e0affc5
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-09-06 18:12:03 +0800

    Updated parser class and refactored Pep's run()
    Parser now includes todo, deadline and event commands.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,6 +12,13 @@ public class Parser {
             return new UnmarkCommand(index);
         } else if (userInput.startsWith("todo ")) {
             return new AddTodoCommand(userInput.substring(5));
+        } else if (userInput.startsWith("deadline ")) {
+            String[] parts = userInput.substring(9).split(" /by ", 2);
+            return new AddDeadlineCommand(parts[0], parts[1]);
+        } else if (userInput.startsWith("event ")) {
+            String[] parts = userInput.substring(6).split(" /from ", 2);
+            String[] timeParts = parts[1].split(" /to ", 2);
+            return new AddEventCommand(parts[0], timeParts[0], timeParts[1]);
         }
         throw new IllegalArgumentException("Unknown command");
     }
--- src/main/java/Pep.java
+++ src/main/java/Pep.java
@@ -15,48 +15,17 @@ public class Pep {
         Scanner scanner = new Scanner(System.in);
         ui.showWelcome(chatbotName);
 
-        String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                ui.showTaskList(taskList);
-            } else if (userInput.startsWith("mark ")) {
-                int index = parseIndex(userInput);
-                if (index != -1 && taskList.markTask(index)) {
-                    ui.showMarked(taskList.getTask(index));
-                }
-            } else if (userInput.startsWith("unmark ")) {
-                int index = parseIndex(userInput);
-                if (index != -1 && taskList.unmarkTask(index)) {
-                    ui.showUnmarked(taskList.getTask(index));
-                }
-            } else if (userInput.startsWith("todo ")) {
-                String description = userInput.substring(5);
-                Task task = new Todo(description);
-                taskList.addTask(task);
-                ui.showAdded(String.valueOf(task), taskList.getCount());
-            } else if (userInput.startsWith("deadline ")) {
-                String[] parts = userInput.substring(9).split(" /by ", 2);
-                if (parts.length == 2) {
-                    Task task = new Deadline(parts[0], parts[1]);
-                    taskList.addTask(task);
-                    ui.showAdded(String.valueOf(task), taskList.getCount());
-                }
-            } else if (userInput.startsWith("event ")) {
-                String[] parts = userInput.substring(6).split(" /from ", 2);
-                if (parts.length == 2) {
-                    String[] timeParts = parts[1].split(" /to ", 2);
-                    if (timeParts.length == 2) {
-                        Task task = new Event(parts[0], timeParts[0], timeParts[1]);
-                        taskList.addTask(task);
-                        ui.showAdded(String.valueOf(task), taskList.getCount());
-                    }
-                }
+        boolean isExit = false;
+        while (!isExit) {
+            String userInput = scanner.nextLine();
+            try {
+                Command command = Parser.parse(userInput);
+                command.execute(taskList, ui);
+                isExit = command.isExit();
+            } catch (Exception e) {
+                ui.showError(e.getMessage());
             }
-
-            userInput = scanner.nextLine();
         }
-
-        ui.showGoodbye();
         scanner.close();
     }
 
