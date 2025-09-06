4373ff25e9d4a3492cefa0cc06240469f1a2e160
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-09-06 18:13:00 +0800

    add AddEventCommand class

---
+++ src/main/java/AddEventCommand.java
@@ -0,0 +1,18 @@
+public class AddEventCommand extends Command {
+    private final String description;
+    private final String from;
+    private final String to;
+
+    public AddEventCommand(String description, String from, String to) {
+        this.description = description;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) {
+        Event task = new Event(description, from, to);
+        tasks.addTask(task);
+        ui.showAdded(task.toString(), tasks.getCount());
+    }
+}
