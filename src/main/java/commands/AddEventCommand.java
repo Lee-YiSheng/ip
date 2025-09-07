c3820c28c50c9e73c5260757c76078b7aca60d05
Lee-YiSheng <leeyisheng14@hotmail.com>
2025-09-07 22:28:17 +0800

    Added exception handling for AddEventCommand

--- src/main/java/commands/AddEventCommand.java
+++ src/main/java/commands/AddEventCommand.java
@@ -1,5 +1,6 @@
 package commands;
 
+import exceptions.PepException;
 import tasks.TaskList;
 import tasks.Event;
 import ui.Ui;
@@ -16,8 +17,11 @@ public class AddEventCommand extends Command {
     }
 
     @Override
-    public void execute(TaskList tasks, Ui ui) {
+    public void execute(TaskList tasks, Ui ui) throws PepException {
         Event task = new Event(description, from, to);
+        if (description == null || description.trim().isEmpty()) {
+            throw new PepException("An event needs a description yo, how bout you Try: event <description>");
+        }
         tasks.addTask(task);
         ui.showAdded(task.toString(), tasks.getCount());
     }
